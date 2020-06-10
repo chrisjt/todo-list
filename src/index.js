a0d518affdc425f1a180dcefc378144db7d167e2
Christian Tan <chrisjoshtan@gmail.com>
2020-06-10 19:02:23 +1000

    Basic html layout

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div>
+    <form>
+      <div class="form-group">
+        <label for="inputTodo">To-do item</label>
+        <input type="text" class="form-control" id="inputTodo" placeholder="Enter to-do item" />
+      </div>
+      <button type="submit" class="btn btn-primary">Add</button>
+    </form>
+    <br/>
+    <ul>
+      <li>Item 1</li>
+    </ul>
+  </div>,
   document.getElementById('container')
 );

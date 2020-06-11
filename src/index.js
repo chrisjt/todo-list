e619190fc40953df7c792e1f93d219e4c243f864
Christian Tan <chrisjoshtan@gmail.com>
2020-06-11 15:50:14 +1000

    Created 'Container' React component class

--- src/index.js
+++ src/index.js
@@ -9,22 +9,36 @@ function Item(props) {
   )
 }
 
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <div class="form-group">
+            <label for="inputTodo">To-do item</label>
+            <input type="text" class="form-control" id="inputTodo" placeholder="Enter to-do item" />
+          </div>
+          <button type="submit" class="btn btn-primary">Add</button>
+        </form>
+        <br/>
+        <ul>
+          <Item item={item} />
+          <Item item={item} />
+        </ul>
+      </div>
+    )
+  }
+}
+
 const item = "Create a website";
 
 ReactDOM.render(
-  <div>
-    <form>
-      <div class="form-group">
-        <label for="inputTodo">To-do item</label>
-        <input type="text" class="form-control" id="inputTodo" placeholder="Enter to-do item" />
-      </div>
-      <button type="submit" class="btn btn-primary">Add</button>
-    </form>
-    <br/>
-    <ul>
-      <Item item={item} />
-      <Item item={item} />
-    </ul>
-  </div>,
+  <Container />,
   document.getElementById('container')
 );
